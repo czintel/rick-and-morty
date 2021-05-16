f0ddecf6f0719716eb4099185eb7cd75074b5a25
Christian Zintel <c.zintel@yahoo.de>
2021-05-17 00:32:34 +0200

    finalized bookmark toggle function

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,26 @@
 import './Card.css'
+import bma from './assets/images/bookmark-active.png'
+import bmi from './assets/images/bookmark-inactive.png'
+
 import { useState } from 'react'
 
 export default function Card({ image, name, species, origin }) {
   const [isActive, setIsActive] = useState(false)
+  const [isBookmarked, setIsBookmarked] = useState(false)
+  console.log(bmi)
 
   return (
     <section className="Card">
+      <img
+        className="Card__Bookmark"
+        src={isBookmarked ? bma : bmi}
+        alt=""
+        onClick={() => setIsBookmarked(!isBookmarked)}
+      />
       <img className="Card__Image" src={image} alt="" />
       <h2 className="Card__Name">{name}</h2>
       <button className="Card__Button" onClick={() => setIsActive(!isActive)}>
-        {isActive ? 'Hide Info' : 'More Info!'}
+        {isActive ? 'Hide!' : 'More Info!'}
       </button>
       {isActive && (
         <p className="Card__Bio">
